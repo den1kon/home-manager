c7718d6a8a962e801bed0bea7ba6c865882d25ce
den1kon <dkondratiuk06@gmail.com>
2025-12-10 17:16:54 +0100

    add file

--- home.nix
+++ home.nix
@@ -3,12 +3,15 @@
 	home = {
 		packages = with pkgs; [
 			hello
-			cowsay lolcat
 		];
 
 		username = "dk";
 		homeDirectory = "/home/dk";
 
 		stateVersion = "25.05";
+
+		file = {
+			"hello.txt".text = "Hello, World!";
+		};
 	};
 }
