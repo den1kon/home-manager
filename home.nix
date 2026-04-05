7f5d5b19bd175f5f0c053246bef795cd3e846600
Denys Kondratiuk <denyskondratiuk@check24.de>
2026-04-05 20:00:53 +0200

    refactor

--- home.nix
+++ home.nix
@@ -1,23 +1,29 @@
-{ config, pkgs, username, system, ... }:
+{
+  config,
+  pkgs,
+  username,
+  system,
+  ...
+}:
 
 let
   php = pkgs.php82.buildEnv {
-    extensions = ({ enabled, all }: enabled ++ (with all; [
-      xdebug
-      imagick
-      redis
-      xsl
-    ]));
+    extensions = (
+      { enabled, all }:
+      enabled
+      ++ (with all; [
+        xdebug
+        imagick
+        redis
+        xsl
+      ])
+    );
     extraConfig = ''
       xdebug.mode=debug
     '';
   };
 
-  homeDirectory = 
-    if system == "aarch64-darwin" then
-      "/Users/${username}"
-    else
-      "/home/${username}";
+  homeDirectory = if system == "aarch64-darwin" then "/Users/${username}" else "/home/${username}";
 
   isMacbook = system == "aarch64-darwin";
   isThinkpad = system == "x86_64-linux";
@@ -34,7 +40,6 @@ in
   # Universal packages for both systems
   home.packages = [
     pkgs.neovim
-    pkgs.ripgrep
     pkgs.tmux
     pkgs.oh-my-zsh
     pkgs.imagemagick
@@ -42,21 +47,55 @@ in
     pkgs.nodejs_24
     pkgs.adminer
     pkgs.lazygit
+
+    pkgs.markdown-oxide
+    pkgs.lua-language-server
+    pkgs.typescript-language-server
+    pkgs.stylua
+    pkgs.nil
+    pkgs.nixfmt
+    pkgs.prettierd
+    pkgs.jq
+    pkgs.php82Packages.php-cs-fixer
+    pkgs.phpactor
+    pkgs.tinymist
+    pkgs.typstyle
+    # pkgs.tree-sitter
+
+    pkgs.rustc
+    pkgs.rustfmt
+    pkgs.cargo
+
+    pkgs.ripgrep
+    pkgs.fd
+    pkgs.bat
+    pkgs.eza
+    pkgs.zoxide
   ]
   # Macbook-specific packages
-  ++ (if isMacbook then [
-    pkgs.jetbrains.phpstorm
-    pkgs.code-cursor
-    pkgs.orbstack
-    php
-    (pkgs.php82Packages.composer.override {
-      inherit php;
-    })
-  ] else [])
+  ++ (
+    if isMacbook then
+      [
+        pkgs.jetbrains.phpstorm
+        pkgs.code-cursor
+        pkgs.orbstack
+        php
+        (pkgs.php82Packages.composer.override {
+          inherit php;
+        })
+      ]
+    else
+      [ ]
+  )
   # Thinkpad-specific packages
-  ++ (if isThinkpad then [
-    pkgs.spotify-player
-  ] else []);
+  ++ (
+    if isThinkpad then
+      [
+        pkgs.spotify-player
+      ]
+    else
+      [ ]
+  );
 
   home.file = {
     # Add file management here if needed
